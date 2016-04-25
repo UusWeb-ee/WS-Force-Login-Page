f6dcc2a803e282e42ac4c681fc5c292320a2e2bc
Silver <Silver@webshark-e5450.lan>
2016-04-25 17:49:39 +0300

    File added

---
+++ ws-force-login-page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Plugin Name: WS Force Login page
+* Plugin URI: http://www.webshark.ee/
+* Description: Redirecting user to login page if not logged in
+* Version: 1.1
+* Author: WebShark
+* Author URI: http://www.webshark.ee/
+**/
+
+
+class WS_Force_Login_Page {
+    public function __construct(){
+        add_action( 'plugins_loaded', array( $this, 'check_if_user_logged_in' ) );
+    }
+
+    public function check_if_user_logged_in(){
+        if (!is_super_admin()) {
+			global $pagenow;
+			if ( 'wp-login.php' !== $pagenow ){
+				wp_redirect(wp_login_url());
+				exit;
+			}
+		}
+    }
+}
+$wpse_ws_force_login_page_plugin = new WS_Force_Login_Page();
+?>
